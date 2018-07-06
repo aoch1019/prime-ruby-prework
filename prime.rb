954a9ad4bc59b7ab761dbe2e8f0f36f44102d943
Andrew Och <andrewoch95@gmail.com>
2018-07-06 03:05:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,8 @@ def prime?(number)
   array_range = (2...number).to_a
   
   array_range.each do |divisor|
-    if number/divisor 
+    if number % divisor == 0
+      return false
   end
+  return true
 end
